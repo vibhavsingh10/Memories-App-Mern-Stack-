8f97f1b9d8f29cb8ddda16143257feebcd8ed437
vibhavsingh10 <55938199+vibhavsingh10@users.noreply.github.com>
2021-07-31 23:53:18 +0530

    Add files via upload

---
+++ Client/build/precache-manifest.6909fa05c1a8cc22722d0750490359e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6808577e9303ba08098062a374bd7c7b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25f2c4f29f5c797528de",
+    "url": "/static/css/main.4e89baf1.chunk.css"
+  },
+  {
+    "revision": "20ae1808e1c9af56956c",
+    "url": "/static/js/2.bfb52602.chunk.js"
+  },
+  {
+    "revision": "90377550dd86ed303dd30d8f487872de",
+    "url": "/static/js/2.bfb52602.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25f2c4f29f5c797528de",
+    "url": "/static/js/main.d2100107.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "5c0c63fb62ec323a10598d7e9de4609e",
+    "url": "/static/media/memories.5c0c63fb.png"
+  }
+]);
